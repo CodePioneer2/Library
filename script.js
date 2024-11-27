62265578be8bae2c34e5ad1bed95c3790ad44e48
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2024-11-27 22:47:03 +0200

    Added book input and display library

--- script.js
+++ script.js
@@ -8,17 +8,40 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render();
 }
 
+function render() {
+  let cards = document.querySelector('#cards');
+  cards.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement('div');
+    bookEl.innerHTML = `<p>${book.title}</p>
+    <p>${book.author}</p>
+    <p>${book.pages}</p>
+    <p>${book.read ? 'Read' : 'Not read'}</p>`;
+    cards.appendChild(bookEl);
+  }
+}
 const newBookBtn = document.querySelector('#new-book-button');
+const newBookForm = document.querySelector('#new-book-form');
+const closeBtn = document.querySelector('#close-form-button');
+
 newBookBtn.addEventListener('click', () => {
-  const newBookForm = document.querySelector('#new-book-form');
   newBookForm.style.display = 'flex';
-  const closeBtn = document.querySelector('#close-form-button');
-  closeBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log(e);
-    newBookForm.style.display = 'none';
-  });
+});
+closeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  newBookForm.style.display = 'none';
+});
+newBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
 });
